7e15a9177260f48c574eaffe84ca9772aad17cf5
Valentina Bautista <valentina_bautista@hotmail.com>
2021-09-30 21:49:00 -0500

    organizar carpeta account

---
+++ src/navigation/AccountStack.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { View, Text } from "react-native";
+import Account from "../screens/Account/Account";
+
+const Stack = createStackNavigator();
+
+export default function AccountStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="account"
+        component={Account}
+        options={{ title: "Cuenta", headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
---
+++ src/screens/Account/Account.js
@@ -0,0 +1,32 @@
+import React, { useState, useCallback } from "react";
+import { ScrollView, Text } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import Search from "../../components/Search";
+import { getMeApi } from "../../api/user";
+import useAuth from "../../hooks/useAuth";
+import StatusBar from "../../components/StatusBar";
+import colors from "../../styles/colors";
+
+export default function Account() {
+  const [user, setUser] = useState(null);
+  const { auth } = useAuth();
+  console.log(auth);
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        const response = await getMeApi(auth.token);
+        setUser(response);
+      })();
+    }, [])
+  );
+  return (
+    <>
+      <StatusBar backgroundColor={colors.bgDark} barStyle="light-content" />
+      <Search />
+      <ScrollView>
+        <Text>Estamos en Mi cuenta</Text>
+      </ScrollView>
+    </>
+  );
+}
